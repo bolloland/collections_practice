37512bcf9369ddec9c26e665410ce1cbfebbab82
ryan.bollettino@gmail.com <bollo@bollo-laptop.localdomain>
2020-11-19 02:41:50 -0600

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,59 @@
+def sort_array_asc(integers)
 
+array = integers.to_a
+array.sort 
+
+end
+
+def sort_array_desc(integers)
+    array = integers.to_a
+    array.sort do |a, b|
+        if a == b
+            0
+        elsif a < b
+            1
+        elsif a > b
+            -1
+        end
+    end
+end
+
+def sort_array_char_count(strings)
+
+strings.sort do |a, b|
+    
+        if a.length == b.length
+          0
+        elsif a.length < b.length
+          -1
+        elsif a.length > b.length
+          1
+        end
+      end 
+    end
+
+def swap_elements(array)
+        array[1],array[2] = array[2],array[1]
+        array
+end        
+   
+def reverse_array(integers)
+    integers.reverse
+end
+
+def kesha_maker(strings)
+strings.each {|dolla| dolla[2] = "$"}
+end
+
+def find_a(words)
+    words.select {|x| x.start_with?("a")}
+end
+
+def sum_array(integers)
+    integers.sum
+end
+
+def add_s(array)
+    array.each_with_index.collect {|word, index| index == 1 ? word = word : word + "s"}
+   
+end
